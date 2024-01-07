6e759864cbcd38df6519c1a6a64ddace9b5d14e3
xuche123 <xuche1998@gmail.com>
2024-01-07 20:49:50 +0800

    implement validation

--- cmd/api/books.go
+++ cmd/api/books.go
@@ -3,22 +3,36 @@ package main
 import (
 	"fmt"
 	"github.com/xuche123/bookwise/internal/data"
+	"github.com/xuche123/bookwise/internal/validator"
 	"net/http"
 	"time"
 )
 
 func (app *application) postBookHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title         string    `json:"title"`
-		Author        string    `json:"author"`
-		PublishedDate time.Time `json:"published_date"`
-		ImageURL      string    `json:"image_url"`
-		Description   string    `json:"description"`
+		Title       string `json:"title"`
+		Author      string `json:"author"`
+		ImageURL    string `json:"image_url"`
+		Description string `json:"description"`
 	}
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	book := &data.Book{
+		Title:       input.Title,
+		Author:      input.Author,
+		ImageURL:    input.ImageURL,
+		Description: input.Description,
+	}
+
+	v := validator.New()
+
+	if data.ValidateBook(v, book); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
@@ -36,14 +50,13 @@ func (app *application) getBookHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	book := data.Book{
-		ID:            id,
-		Title:         "Test book",
-		Author:        "Test author",
-		PublishedDate: time.Time{},
-		ImageURL:      "-",
-		Description:   "Test description",
-		CreatedAt:     time.Now(),
-		Version:       1,
+		ID:          id,
+		Title:       "Test book",
+		Author:      "Test author",
+		ImageURL:    "-",
+		Description: "Test description",
+		CreatedAt:   time.Now(),
+		Version:     1,
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"book": book}, nil)
--- internal/data/books.go
+++ internal/data/books.go
@@ -1,14 +1,27 @@
 package data
 
-import "time"
+import (
+	"github.com/xuche123/bookwise/internal/validator"
+	"time"
+)
 
 type Book struct {
-	ID            int64     `json:"id"`
-	Title         string    `json:"title"`
-	Author        string    `json:"author"`
-	PublishedDate time.Time `json:"published_date,omitempty"`
-	ImageURL      string    `json:"image_url,omitempty"`
-	Description   string    `json:"description,omitempty"`
-	CreatedAt     time.Time `json:"-"`
-	Version       int32     `json:"version"`
+	ID          int64     `json:"id"`
+	Title       string    `json:"title"`
+	Author      string    `json:"author"`
+	ImageURL    string    `json:"image_url,omitempty"`
+	Description string    `json:"description,omitempty"`
+	CreatedAt   time.Time `json:"-"`
+	Version     int32     `json:"version"`
+}
+
+func ValidateBook(v *validator.Validator, book *Book) {
+	v.Check(book.Title != "", "title", "must be provided")
+	v.Check(len(book.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(book.Author != "", "author", "must be provided")
+	v.Check(len(book.Author) <= 500, "author", "must not be more than 500 bytes long")
+	v.Check(book.ImageURL != "", "image_url", "must be provided")
+	v.Check(len(book.ImageURL) <= 500, "image_url", "must not be more than 500 bytes long")
+	v.Check(book.Description != "", "description", "must be provided")
+	v.Check(len(book.Description) <= 50000, "description", "must not be more than 50000 bytes long")
 }
